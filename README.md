7321d11997a05f251dc24d03ae1a2ccfa9abf915
nedokaka <104865210+nedokaka@users.noreply.github.com>
2023-11-03 12:27:22 +0300

    first & last commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# adrod 13 installer
+adrod 13 installer source code
+source based on real app but without sketchware gorecode
+*real app was created with sketchware (gui app constructor)*
+
+**this repository will not be updated**
---
+++ app/src/main/java/com/aloapps/adrodinstaller/PhonbrickActivity.java
@@ -0,0 +1,88 @@
+package com.aloapps.adrodinstaller;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.SparseBooleanArray;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class PhonbrickActivity extends Activity {
+    private ImageView imageview1;
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.phonbrick);
+        initialize(bundle);
+        initializeLogic();
+    }
+
+    private void initialize(Bundle bundle) {
+        this.imageview1 = (ImageView) findViewById(R.id.imageview1);
+    }
+
+    private void initializeLogic() {
+    }
+
+    @Override
+    protected void onActivityResult(int i, int i2, Intent intent) {
+        super.onActivityResult(i, i2, intent);
+    }
+
+    @Deprecated
+    public void showMessage(String str) {
+        Toast.makeText(getApplicationContext(), str, 0).show();
+    }
+
+    @Deprecated
+    public int getLocationX(View view) {
+        int[] iArr = new int[2];
+        view.getLocationInWindow(iArr);
+        return iArr[0];
+    }
+
+    @Deprecated
+    public int getLocationY(View view) {
+        int[] iArr = new int[2];
+        view.getLocationInWindow(iArr);
+        return iArr[1];
+    }
+
+    @Deprecated
+    public int getRandom(int i, int i2) {
+        return new Random().nextInt((i2 - i) + 1) + i;
+    }
+
+    @Deprecated
+    public ArrayList<Double> getCheckedItemPositionsToArray(ListView listView) {
+        ArrayList<Double> arrayList = new ArrayList<>();
+        SparseBooleanArray checkedItemPositions = listView.getCheckedItemPositions();
+        for (int i = 0; i < checkedItemPositions.size(); i++) {
+            if (checkedItemPositions.valueAt(i)) {
+                arrayList.add(Double.valueOf(checkedItemPositions.keyAt(i)));
+            }
+        }
+        return arrayList;
+    }
+
+    @Deprecated
+    public float getDip(int i) {
+        return TypedValue.applyDimension(1, i, getResources().getDisplayMetrics());
+    }
+
+    @Deprecated
+    public int getDisplayWidthPixels() {
+        return getResources().getDisplayMetrics().widthPixels;
+    }
+
+    @Deprecated
+    public int getDisplayHeightPixels() {
+        return getResources().getDisplayMetrics().heightPixels;
+    }
+}
